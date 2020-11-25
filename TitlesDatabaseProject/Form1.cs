a02501674e0e2ac28a819f15d41ba62f550425d0
ITICLASSROOM\jromo021627 <romo17410@gmail.com>
2020-11-24 19:37:04 -0600

    Add project files.

---
+++ TitlesDatabaseProject/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TitlesDatabaseProject
+{
+    public partial class frmTitles : Form
+    {
+        SqlConnection booksConnection;
+
+
+        public frmTitles()
+        {
+            InitializeComponent();
+        }
+
+        private void frmTitles_Load(object sender, EventArgs e)
+        {
+            
+            booksConnection = new SqlConnection("Server=(localdb)\\MSSQLLocalDB;"
+                                    + "AttachDbFilename=C:\\VCSDB\\Databases\\SQLBooksDB.mdf;"
+                                    + "Integrated Security=True;"
+                                    + "Connect Timeout=30;");
+            booksConnection.Open();
+            lblState.Text = booksConnection.State.ToString();
+            booksConnection.Close();
+            lblState.Text += booksConnection.State.ToString();
+            booksConnection.Dispose();
+        }
+    }
+}
