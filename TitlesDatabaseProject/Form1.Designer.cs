f9911549fdcaa7de6b95369d8b8a2d49063f089f
ITICLASSROOM\jromo021627 <romo17410@gmail.com>
2020-12-01 20:37:19 -0600

    Started to work on data bindings 3-1

--- TitlesDatabaseProject/Form1.Designer.cs
+++ TitlesDatabaseProject/Form1.Designer.cs
@@ -28,36 +28,124 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblState = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtYearPublished = new System.Windows.Forms.TextBox();
+            this.txtISBN = new System.Windows.Forms.TextBox();
+            this.txtPubID = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // lblState
+            // lblTitle
             // 
-            this.lblState.BackColor = System.Drawing.Color.White;
-            this.lblState.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.lblState.Location = new System.Drawing.Point(12, 405);
-            this.lblState.Name = "lblState";
-            this.lblState.Size = new System.Drawing.Size(665, 36);
-            this.lblState.TabIndex = 0;
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(31, 15);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(27, 13);
+            this.lblTitle.TabIndex = 1;
+            this.lblTitle.Text = "Title";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Year Published";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "ISBN";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Publisher ID";
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.BackColor = System.Drawing.Color.White;
+            this.txtTitle.Location = new System.Drawing.Point(134, 12);
+            this.txtTitle.Multiline = true;
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.ReadOnly = true;
+            this.txtTitle.Size = new System.Drawing.Size(295, 37);
+            this.txtTitle.TabIndex = 5;
+            // 
+            // txtYearPublished
+            // 
+            this.txtYearPublished.BackColor = System.Drawing.Color.White;
+            this.txtYearPublished.Location = new System.Drawing.Point(134, 55);
+            this.txtYearPublished.Multiline = true;
+            this.txtYearPublished.Name = "txtYearPublished";
+            this.txtYearPublished.ReadOnly = true;
+            this.txtYearPublished.Size = new System.Drawing.Size(295, 37);
+            this.txtYearPublished.TabIndex = 9;
+            // 
+            // txtISBN
+            // 
+            this.txtISBN.BackColor = System.Drawing.Color.White;
+            this.txtISBN.Location = new System.Drawing.Point(134, 98);
+            this.txtISBN.Multiline = true;
+            this.txtISBN.Name = "txtISBN";
+            this.txtISBN.ReadOnly = true;
+            this.txtISBN.Size = new System.Drawing.Size(295, 37);
+            this.txtISBN.TabIndex = 10;
+            // 
+            // txtPubID
+            // 
+            this.txtPubID.BackColor = System.Drawing.Color.White;
+            this.txtPubID.Location = new System.Drawing.Point(134, 141);
+            this.txtPubID.Multiline = true;
+            this.txtPubID.Name = "txtPubID";
+            this.txtPubID.ReadOnly = true;
+            this.txtPubID.Size = new System.Drawing.Size(295, 37);
+            this.txtPubID.TabIndex = 11;
             // 
             // frmTitles
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(689, 450);
-            this.Controls.Add(this.lblState);
+            this.ClientSize = new System.Drawing.Size(467, 255);
+            this.Controls.Add(this.txtPubID);
+            this.Controls.Add(this.txtISBN);
+            this.Controls.Add(this.txtYearPublished);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblTitle);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "frmTitles";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Tiles Database";
             this.Load += new System.EventHandler(this.frmTitles_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label lblState;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtYearPublished;
+        private System.Windows.Forms.TextBox txtISBN;
+        private System.Windows.Forms.TextBox txtPubID;
     }
 }
 
