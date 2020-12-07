114ec41501821ccd33ea5c3a745fe97d3bff2917
jkr174 <romo17410@gmail.com>
2020-12-07 17:55:20 -0600

    Update Form1.cs
    Finished Databindings from the book, and now it has error handling for when the program cannot find the database file.

--- TitlesDatabaseProject/Form1.cs
+++ TitlesDatabaseProject/Form1.cs
@@ -31,49 +31,50 @@ namespace TitlesDatabaseProject
 
         private void frmTitles_Load(object sender, EventArgs e)
         {
-            /*try
-            {
-                if (!File.Exists("SQLBooksDB.mdf"))
-                    throw new FileNotFoundException();
-            }
-            catch(Exception e)
-            {
-                MessageBox.Show("The file does not exist!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                WriteException(e);
-            }*/
+            bool canAccessDB = false;
+            var database = "SQLBooksDB.mdf";
 
             booksConnection = new SqlConnection("Server=(localdb)\\MSSQLLocalDB;"
-                                    + "AttachDbFilename='|DataDirectory|\\SQLBooksDB.mdf';"
+                                    + "AttachDbFilename=|DataDirectory|\\" + database + ";"
                                     + "Integrated Security=True;"
                                     + "Connect Timeout=30;");
-            booksConnection.Open();
 
-            SqlCommand titlesCommand = new SqlCommand("Select * from Titles", booksConnection);
-            SqlDataAdapter titlesAdapter = new SqlDataAdapter();
-            titlesAdapter.SelectCommand = titlesCommand;
-            DataTable titlesTable = new DataTable();
-            titlesAdapter.Fill(titlesTable);
-
-            txtTitle.DataBindings.Add("Text", titlesTable, "Title");
-            txtYearPublished.DataBindings.Add("Text", titlesTable, "Year_Published");
-            txtISBN.DataBindings.Add("Text", titlesTable, "ISBN");
-            txtPubID.DataBindings.Add("Text", titlesTable, "PubID");
-            
-            booksConnection.Close();
-            booksConnection.Dispose();
-            titlesCommand.Dispose();
-            titlesAdapter.Dispose();
-            titlesTable.Dispose();
-        }
-        public static void WriteException(Exception exception)
-        {
-            string filePath = @"|DataDirectory|//Error.txt";
-
-            using (StreamWriter logWriter = new StreamWriter(filePath, true))
+            try
             {
-                logWriter.WriteLine("Message :" + exception.Message + "<br/>" + Environment.NewLine + "StackTrace :" + exception.StackTrace +
-                    "" + Environment.NewLine + "Date :" + DateTime.Now.ToString());
-                logWriter.WriteLine(Environment.NewLine + "----------------------");
+                booksConnection.Open();
+
+                SqlCommand titlesCommand = new SqlCommand("Select * from Titles", booksConnection);
+                SqlDataAdapter titlesAdapter = new SqlDataAdapter();
+                titlesAdapter.SelectCommand = titlesCommand;
+                DataTable titlesTables = new DataTable();
+                titlesAdapter.Fill(titlesTables);
+
+                txtTitle.DataBindings.Add("Text", titlesTables, "Title");
+                txtYearPublished.DataBindings.Add("Text", titlesTables, "Year_Published");
+                txtISBN.DataBindings.Add("Text", titlesTables, "ISBN");
+                txtPubID.DataBindings.Add("Text", titlesTables, "PubID");
+
+                booksConnection.Close();
+
+                booksConnection.Dispose();
+                titlesCommand.Dispose();
+                titlesAdapter.Dispose();
+                titlesTables.Dispose();
+
+                canAccessDB = true;
+            }
+            catch
+            {
+                if (!canAccessDB)
+                {
+                    const string message = "Cannot connect to database. Please check if SQLBooksDB.mdf is in the bin folder!";
+                    const string caption = "Cannot find database!";
+
+                    MessageBox.Show(message,
+                        caption,
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                }
             }
         }
     }
